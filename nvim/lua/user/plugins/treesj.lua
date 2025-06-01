25dd6cfb3df665968d91f741877b6b33e08de472
rc100808 <rishav.chakraborty@oracle.com>
2025-06-01 23:55:28 +0530

    feature add

---
+++ nvim/lua/user/plugins/treesj.lua
@@ -0,0 +1,12 @@
+return {
+	"Wansmer/treesj",
+	keys = { "<space>m" },
+	dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  }, -- if you install parsers with `nvim-treesitter`
+	config = function()
+		require("treesj").setup({--[[ your config ]]
+       max_join_length = 180,
+		})
+	end,
+}
